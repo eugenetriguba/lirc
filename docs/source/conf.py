48f2a25ddcb02fa188af879882a9125a798d1e1e
Eugene Triguba <eugenetriguba@gmail.com>
2020-12-26 15:18:43 -0600

    Bump version: 0.2.0 → 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Eugene Triguba"
 author = "Eugene Triguba"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
