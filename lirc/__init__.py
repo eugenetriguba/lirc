b9cf7b42237b73fc612a514c5b26fa8dcbf8f002
Eugene Triguba <eugenetriguba@gmail.com>
2021-04-18 18:56:56 -0500

    Bump version: 1.0.1 → 2.0.0

--- lirc/__init__.py
+++ lirc/__init__.py
@@ -1,6 +1,6 @@
 from lirc.client import Client
 from lirc.connection.lircd_connection import LircdConnection
 
-__version__ = "1.0.1"
+__version__ = "2.0.0"
 
 __all__ = ["Client", "LircdConnection"]
