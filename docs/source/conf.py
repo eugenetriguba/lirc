b0265a0911d3e01348bce38c5078f9ac2d69d16c
Eugene Triguba <eugenetriguba@gmail.com>
2022-11-25 12:45:41 -0500

    Bump version: 2.0.1 → 2.0.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, Eugene Triguba"
 author = "Eugene Triguba"
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.1"
+release = "2.0.2"
 
 
 # -- General configuration ---------------------------------------------------
--- lirc/__init__.py
+++ lirc/__init__.py
@@ -1,6 +1,6 @@
 from lirc.client import Client
 from lirc.connection.lircd_connection import LircdConnection
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 __all__ = ["Client", "LircdConnection"]
